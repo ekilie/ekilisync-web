6a5eaf2897dce48b32ca1b4a27172499ffa94ac3
tacherasasi <tacherasasi@gmail.com>
2025-07-16 15:25:27 +0300

    remove unused Apps import from apps route

--- src/routes/_authenticated/apps/index.tsx
+++ src/routes/_authenticated/apps/index.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router'
-import Apps from '@/features/apps'
 import ComingSoon from '@/components/coming-soon'
 
 export const Route = createFileRoute('/_authenticated/apps/')({
