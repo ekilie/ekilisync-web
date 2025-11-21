f34c0c7b0ee714ddf98f2fea4cd32750a7aa7815
Tachera Sasi <tacherasasi@gmail.com>
2025-11-21 10:12:45 +0300

    Update Loader component for dark mode support; adjust sidebar primary color for improved accessibility

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,10 +2,10 @@ const Loader = () => {
   return (
     <div className='flex h-screen items-center justify-center'>
       <div className='loader relative h-12 w-12 animate-spin'>
-        <div className='circle absolute top-0 left-0 h-5 w-5 rounded-full bg-gray-800'></div>
-        <div className='circle absolute top-0 right-0 h-5 w-5 rounded-full bg-gray-800'></div>
-        <div className='circle absolute right-0 bottom-0 h-5 w-5 rounded-full bg-gray-800'></div>
-        <div className='circle absolute bottom-0 left-0 h-5 w-5 rounded-full bg-gray-800'></div>
+        <div className='circle absolute top-0 left-0 h-5 w-5 rounded-full bg-gray-800 dark:bg-gray-200'></div>
+        <div className='circle absolute top-0 right-0 h-5 w-5 rounded-full bg-gray-800 dark:bg-gray-200'></div>
+        <div className='circle absolute right-0 bottom-0 h-5 w-5 rounded-full bg-gray-800 dark:bg-gray-200'></div>
+        <div className='circle absolute bottom-0 left-0 h-5 w-5 rounded-full bg-gray-800 dark:bg-gray-200'></div>
       </div>
     </div>
   )
