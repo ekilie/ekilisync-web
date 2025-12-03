4cb4147c348aa7bebb463f0e3b953eb543a9d059
tacheraSasi <tacherasasi@gmail.com>
2025-12-03 13:43:02 +0300

    Add FloatingPaths component to AuthLayout for enhanced visual design

--- src/features/auth/auth-layout.tsx
+++ src/features/auth/auth-layout.tsx
@@ -1,9 +1,14 @@
+import { FloatingPaths } from "@/components/floating-paths"
+
 interface Props {
   children: React.ReactNode
 }
 
 export default function AuthLayout({ children }: Props) {
   return (
+    <>
+    <FloatingPaths position={1} />
+    <FloatingPaths position={-1} />
     <div className='bg-primary-foreground container grid h-svh max-w-none items-center justify-center'>
       <div className='mx-auto flex w-full flex-col justify-center space-y-2 py-8 sm:w-[480px] sm:p-8'>
         <div className='mb-4 flex items-center justify-center'>
@@ -24,5 +29,6 @@ export default function AuthLayout({ children }: Props) {
         {children}
       </div>
     </div>
+    </>
   )
 }
